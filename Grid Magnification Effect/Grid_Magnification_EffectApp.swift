1d27e41cee9f034d9987cc4f71b637f83b8f555d
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-28 19:24:42 +0300

    Initial Commit

---
+++ Grid Magnification Effect/Grid_Magnification_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grid_Magnification_EffectApp.swift
+//  Grid Magnification Effect
+//
+//  Created by Антон Кулик on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Grid_Magnification_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
